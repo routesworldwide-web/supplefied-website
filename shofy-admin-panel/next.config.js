d6cee6dca0d3f3373eeeef00e2486d2116b6fe90
routesworldwide-web <info@routesworldwideexpress.com>
2026-06-04 10:34:43 +0000

    Fix admin deployment config and ignore env files

--- shofy-admin-panel/next.config.js
+++ shofy-admin-panel/next.config.js
@@ -1,22 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/admin",
+
   redirects: async () => {
     return [
       {
-        source: '/',
-        destination: '/login',
+        source: "/",
+        destination: "/login",
         permanent: false,
       },
     ];
   },
+
   images: {
-    domains: ['i.ibb.co', 'res.cloudinary.com', 'lh3.googleusercontent.com', 'localhost'],
+    domains: [
+      "i.ibb.co",
+      "res.cloudinary.com",
+      "lh3.googleusercontent.com",
+      "localhost",
+      "35.200.248.118",
+    ],
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '7000',
-        pathname: '/images/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "7000",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "http",
+        hostname: "35.200.248.118",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "http",
+        hostname: "35.200.248.118",
+        pathname: "/uploads/**",
       },
     ],
   },
